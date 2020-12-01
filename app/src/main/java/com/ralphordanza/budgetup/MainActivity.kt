e34fbc16032cd843719e64ff8e12c6743e5c0f2e
Ralph Ordanza <mendelordanza@gmail.com>
2020-12-01 16:08:04 +0800

    :lipstick: Add bottom app bar

--- app/src/main/java/com/ralphordanza/budgetup/MainActivity.kt
+++ app/src/main/java/com/ralphordanza/budgetup/MainActivity.kt
@@ -2,10 +2,26 @@ package com.ralphordanza.budgetup
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ralphordanza.budgetup.databinding.ActivityMainBinding
+import splitties.toast.toast
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        attachActions()
     }
+
+    private fun attachActions(){
+        binding.bottomAppBar.setNavigationOnClickListener {
+            toast("Navigation")
+        }
+    }
+
 }
