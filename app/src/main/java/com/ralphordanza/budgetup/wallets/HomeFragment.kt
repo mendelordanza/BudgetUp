7118b6a3a87007c322811e373fc20b5654c2e0d9
Ralph Ordanza <mendelordanza@gmail.com>
2020-12-09 22:24:29 +0800

    :sparkles: Integrate real time database with sample data

--- app/src/main/java/com/ralphordanza/budgetup/wallets/HomeFragment.kt
+++ app/src/main/java/com/ralphordanza/budgetup/wallets/HomeFragment.kt
@@ -7,6 +7,10 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import com.ralphordanza.budgetup.databinding.FragmentHomeBinding
 import com.ralphordanza.budgetup.models.Wallet
 
@@ -43,7 +47,7 @@ class HomeFragment : Fragment() {
     }
 
     private fun setupRecyclerView() {
-        walletAdapter = WalletAdapter(mutableListOf()){
+        walletAdapter = WalletAdapter{
             //On click wallet item
             val action = HomeFragmentDirections.actionHomeFragmentToTransactionsFragment(
                     it
@@ -57,15 +61,40 @@ class HomeFragment : Fragment() {
     }
 
     private fun showDummyData(){
-        val dummyList = mutableListOf<Wallet>()
-        dummyList.add(Wallet("0", "BDO", "PHP 5,000.00"))
-        dummyList.add(Wallet("1", "ING", "PHP 10,000.00"))
-        dummyList.add(Wallet("2", "RBank", "PHP 5,000.00"))
-        dummyList.add(Wallet("3", "Cash", "PHP 9,000.00"))
-        dummyList.add(Wallet("0", "BDO", "PHP 5,000.00"))
-        dummyList.add(Wallet("1", "ING", "PHP 10,000.00"))
-        dummyList.add(Wallet("2", "RBank", "PHP 5,000.00"))
-        dummyList.add(Wallet("3", "Cash", "PHP 9,000.00"))
-        walletAdapter.updateList(dummyList)
+//        val dummyList = mutableListOf<Wallet>()
+//        dummyList.add(Wallet("0", "BDO", "PHP 5,000.00"))
+//        dummyList.add(Wallet("1", "ING", "PHP 10,000.00"))
+//        dummyList.add(Wallet("2", "RBank", "PHP 5,000.00"))
+//        dummyList.add(Wallet("3", "Cash", "PHP 9,000.00"))
+//        dummyList.add(Wallet("0", "BDO", "PHP 5,000.00"))
+//        dummyList.add(Wallet("1", "ING", "PHP 10,000.00"))
+//        dummyList.add(Wallet("2", "RBank", "PHP 5,000.00"))
+//        dummyList.add(Wallet("3", "Cash", "PHP 9,000.00"))
+//        walletAdapter.updateList(dummyList)
+
+        val database = FirebaseDatabase.getInstance()
+        val walletRef = database.getReference("wallets")
+
+        walletRef.addValueEventListener(object : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+//                val list = snapshot.children.map {
+//                    val wallet = snapshot.getValue(Wallet::class.java)
+//                    wallet
+//                }
+                val list = mutableListOf<Wallet>()
+                for (wallet in snapshot.children) {
+                    val item = wallet.getValue(Wallet::class.java)
+                    if (item != null) {
+                        list.add(item)
+                    }
+                }
+                walletAdapter.submitList(list)
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+
+        })
     }
 }
